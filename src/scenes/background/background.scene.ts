5814c483091ba8b0e000eb0b10fc67763553be00
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2024-02-04 03:57:35 -0500

    feat: add animations settings check to background

--- src/scenes/background/background.scene.ts
+++ src/scenes/background/background.scene.ts
@@ -1,5 +1,8 @@
 import { ASSETS, SCENES } from "@constants";
+import { SettingsKeys, SettingsService } from "@services";
 import Phaser from "phaser";
+import { Observable, Subject, takeUntil } from "rxjs";
+import { container } from "tsyringe";
 
 /**
  * Represents the background scene of the game.
@@ -8,22 +11,50 @@ export class BackgroundScene extends Phaser.Scene {
   private waterSprite!: Phaser.GameObjects.Sprite;
   private waterTileSprite!: Phaser.GameObjects.TileSprite;
 
+  private destroy$ = new Subject<void>();
+  private isAnimationsEnabled$: Observable<boolean>;
+
   /**
    * Initializes the background scene.
    */
   constructor() {
     super(SCENES.BACKGROUND);
+
+    const settingsService = container.resolve(SettingsService);
+    this.isAnimationsEnabled$ = settingsService.getSetting$(
+      SettingsKeys.Animations
+    );
   }
 
   /**
    * Creates the background scene.
    */
   create() {
+    this.createBackground();
+
+    this.events.once(Phaser.Scenes.Events.DESTROY, () => {
+      this.destroy$.next();
+      this.destroy$.complete();
+    });
+  }
+
+  /**
+   * Updates the water tile sprite frame.
+   */
+  update() {
+    this.waterTileSprite.setFrame(this.waterSprite.frame.name);
+  }
+
+  /**
+   * Creates the background scene.
+   */
+  public createBackground() {
     const { width, height } = this.scale;
 
     this.waterSprite = this.add
       .sprite(0, 0, ASSETS.SPRITESHEETS.WATER)
-      .setVisible(false);
+      .setVisible(false)
+      .setActive(false);
     this.waterSprite.anims.create({
       key: ASSETS.SPRITESHEETS.WATER,
       frames: this.waterSprite.anims.generateFrameNumbers(
@@ -37,12 +68,11 @@ export class BackgroundScene extends Phaser.Scene {
     this.waterTileSprite = this.add
       .tileSprite(0, 0, width, height, ASSETS.SPRITESHEETS.WATER)
       .setOrigin(0, 0);
-  }
 
-  /**
-   * Updates the water tile sprite frame.
-   */
-  update() {
-    this.waterTileSprite.setFrame(this.waterSprite.frame.name);
+    this.isAnimationsEnabled$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((isAnimationsEnabled) => {
+        this.waterSprite.setActive(isAnimationsEnabled);
+      });
   }
 }
